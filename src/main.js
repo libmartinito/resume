a4643de51d0417545fd9304263593b0c4334e472
Lib Joshua Martinito <lib.martinito@pm.me>
2025-02-09 20:24:06 +0800

    feat: add resume view count

--- src/main.js
+++ src/main.js
@@ -1,2 +1,21 @@
 import './style.css'
 
+const apiGatewayUrl = "https://lib-martinito-resume-api-gateway-2gjc7aij.ts.gateway.dev"
+
+async function getViews() {
+    try {
+        const response = await fetch(`${apiGatewayUrl}/get-views`)
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+
+        const data = await response.json()
+        document.getElementById("view-count").textContent = data.views
+    } catch (e) {
+        console.error("Error fetching view count: ", e)
+        document.getElementById("view-count").textContent = "Error loading views"
+    }
+}
+
+window.addEventListener("DOMContentLoaded", getViews)
